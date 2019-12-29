c508a5e1b582652d9fce000ea887de8f2ecdb0c0
Alan Nguyen <tuan8101@gmail.com>
2019-12-29 12:48:44 -0700

    fix typos

--- expirationAlert.cs
+++ expirationAlert.cs
@@ -1,21 +1,26 @@
+using System;
+
 Random random = new Random();
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
 if (daysUntilExpiration == 0)
-{
+{Sh
     Console.WriteLine("Your subscription has expired.");
 }
+
 else if (daysUntilExpiration == 1)
 {
     Console.WriteLine("Your subscription expires within a day!");
     discountPercentage = 20;
 }
+
 else if (daysUntilExpiration <= 5)
 {
     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
     discountPercentage = 10;
 }
+
 else if (daysUntilExpiration <= 10)
 {
     Console.WriteLine("Your subscription will expire soon.  Renew now!");
